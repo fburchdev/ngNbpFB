c33f8a75521fe187739213ee2c71bbd3bc1e098a
Frances Burch <fburchdev@gmail.com>
2014-08-16 23:18:18 -0400

    display series and books

--- scripts/controllers/main-ctrl.js
+++ scripts/controllers/main-ctrl.js
@@ -1,11 +1,16 @@
 angular.module('authors').controller('MainCtrl',
     ['$scope', '$http', 'AuthorsSvc', function ($scope, $http, authorsSvc) {
 
-        $scope.authors = [];
-        $scope.loadAuthors = function () {
-            authorsSvc.authors().then(function (result) {
-                $scope.authors = result.data;
-            });
-        };
+        //$scope.authors = [];
+        //$scope.loadAuthors = function () {
+          //  authorsSvc.authors().then(function (result) {
+            //    $scope.authors = result.data;
+         //   });
+        //};
+
+        authorsSvc.authors().then(function (result) {
+          $scope.authors = result.data;
+         });
+
 
     }]);
