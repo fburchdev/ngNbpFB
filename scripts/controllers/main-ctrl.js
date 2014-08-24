41dc65acb974d8ec2eb064c573cca6e433ae09d1
Frances Burch <fburchdev@gmail.com>
2014-08-23 22:34:10 -0400

    Toggle Series/Book visibility on/off

--- scripts/controllers/main-ctrl.js
+++ scripts/controllers/main-ctrl.js
@@ -5,5 +5,20 @@ app.controller('MainCtrl',
         var firebaseRef = new Firebase('https://shining-inferno-1424.firebaseio.com/');
         $scope.authors = $firebase(firebaseRef).$asArray();
 
+        $scope.setCurrentAuthor = function(author){
+            $scope.currentAuthor = author;
+        };
+
+        $scope.isCurrentAuthor = function(author){
+            return author === $scope.currentAuthor;
+        };
+
+        $scope.setCurrentSeries = function(series){
+            $scope.currentSeries = series;
+        };
+
+        $scope.isCurrentSeries = function(series){
+            return series === $scope.currentSeries;
+        };
 
     }]);
